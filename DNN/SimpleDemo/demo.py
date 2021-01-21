ee7582694fd59012b91221464ccdd5fc9a9d64cb
Hongbin Mao <hello2mao@gmail.com>
2021-01-21 17:50:44 +0800

    Lazy push: 2021年 1月21日 星期四 17时50分44秒 CST

---
+++ DNN/SimpleDemo/demo.py
@@ -0,0 +1,5 @@
+from NeuralNetwork import NeuralNetwork
+nn = NeuralNetwork(2, 2, 2, hidden_layer_weights=[0.15, 0.2, 0.25, 0.3], hidden_layer_bias=0.35, output_layer_weights=[0.4, 0.45, 0.5, 0.55], output_layer_bias=0.6)
+for i in range(10000):
+    nn.train([0.05, 0.1], [0.01, 0.99])
+    print(i, round(nn.calculate_total_error([[[0.05, 0.1], [0.01, 0.99]]]), 9))
