ac9fb4c4da2831404ee21a19eb672d7fb643d55a
Hongbin Mao <hello2mao@gmail.com>
2021-01-21 12:01:29 +0800

    Lazy push: 2021年 1月21日 星期四 12时01分29秒 CST

---
+++ DecisionTree/SklearnDemo/demo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from sklearn import tree
+X = [[0, 0], [1, 1]]
+Y = [0, 1]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, Y)
+print(clf.predict([[2., 2.]]))
