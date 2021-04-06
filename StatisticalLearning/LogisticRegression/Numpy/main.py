1c09b02c6dd2edef464efd4875d4658b6d053b6d
Hongbin Mao <hello2mao@gmail.com>
2021-04-06 11:41:58 +0800

    Lazy push: Tue Apr  6 11:41:58 CST 2021

--- StatisticalLearning/LogisticRegression/Numpy/main.py
+++ StatisticalLearning/LogisticRegression/Numpy/main.py
@@ -3,12 +3,18 @@ import pandas as pd
 import random
 
 
+# min-max 归一化
+def normalization(data):
+    _range = np.max(data) - np.min(data)
+    return (data - np.min(data)) / _range
+
+
 # 加载数据
 def load_data():
     df = pd.read_csv('data.csv')
     features = df.iloc[:, :-1].to_numpy()
-    labels = df.iloc[:, -1:].to_numpy().reshape(1, -1)[0]
-    return features, labels
+    labels = np.squeeze(df.iloc[:, -1:].to_numpy().reshape(1, -1))
+    return normalization(features), normalization(labels)
 
 
 # 批量读取数据
@@ -24,8 +30,9 @@ def data_iter(batch_size, X, y):
 # 计算损失
 def compute_loss(X, y, w, b):
     y_hat = sigmoid(np.dot(X, w) + b)
-    print('y_hat', 1-y_hat)
-    return (y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat)).sum()
+    loss = -np.sum(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat))
+    loss /= len(X)
+    return loss
 
 
 # 计算梯度
@@ -34,7 +41,7 @@ def compute_gradient(X, y, w, b):
 
 
 def sigmoid(x):
-    return 1.0 / (1 + np.exp(-x))
+    return 1.0 / (1.0 + np.exp(-x))
 
 
 def fit(X, y):
@@ -42,17 +49,21 @@ def fit(X, y):
     w = np.random.rand(X.shape[1])
     b = np.random.rand(1)
     print(f'init w: {w}, b: {b}')
+
     # 开始训练
     batch_size = 10
     learning_rate = 0.1
     iter_max = 1
     for n_iter in range(1, iter_max+1):
+        # compute loss
+        loss = compute_loss(X, y, w, b)
+        if abs(loss) <= 1e-4:
+            break
         for (batch_X, batch_y) in data_iter(batch_size, X, y):
             print(f'current batch_X: {batch_X}')
             print(f'current batch_y: {batch_y}')
 
-            # compute loss
-            loss = compute_loss(X, y, w, b)
+
             print(loss)
             return
 
